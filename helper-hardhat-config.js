7a65d14bcefa1e25104efe178a8452b713c59d64
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-05-28 21:55:31 +0700

    hardhat configuration

---
+++ helper-hardhat-config.js
@@ -0,0 +1,11 @@
+const networkConfig = {
+    31337: {
+        name: "hardhat",
+    },
+    11155111: {
+        name: "sepolia",
+        ethTousdpricefeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+};
+
+module.exports = { networkConfig };
